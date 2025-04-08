42006ecdbaae05d35d4edd8c095844763b877115
qiaoyx-or <openaps@163.com>
2025-04-08 15:31:42 +0800

    init

---
+++ __init__.py
@@ -0,0 +1,3 @@
+# Pyarmor 8.5.9 (pro), 005890, 2025-04-08T15:09:44.359873
+from sys import version_info as py_version
+__pyarmor__ = __import__("py%d%d.pyarmor_runtime" % py_version[:2], globals(), locals(), ["__pyarmor__"], 1).__pyarmor__
